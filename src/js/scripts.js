d44dc92ca2482233a223af35f07e074622a8c370
Zykava Alena <zykava.alena@gmail.com>
2021-04-30 16:56:41 +0300

    innit: layout

---
+++ src/js/scripts.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+
+// Global jQuery variables
+global.jQuery = $;
+global.$ = $;
+
+// Example import owl-carousel
+// let owl_carousel = require('owl.carousel');
+// window.fn = owl_carousel;
+
+$(document).ready(()=>{
+
+  // Write comment here ------------------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------------------------------------
+
+});
